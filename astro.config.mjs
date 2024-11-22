48fe6dc8ebf3674a063d4f8c04475d07756c2d4d
DaniAkash <s.daniakash@gmail.com>
2024-11-22 20:50:40 +0530

    feat: added cloudflare support

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: 'server',
+  adapter: cloudflare()
 });
