666acd9328af70bd8fe2ad59f5c0cbff4f362e1c
DaniAkash <s.daniakash@gmail.com>
2024-11-22 23:01:04 +0530

    feat: use the tool invocation result

--- src/pages/api/ai.ts
+++ src/pages/api/ai.ts
@@ -1,6 +1,7 @@
 import type { APIRoute } from "astro";
 import { createOpenAI } from "@ai-sdk/openai";
-import { streamText, type CoreMessage } from "ai";
+import { convertToCoreMessages, streamText, type Message } from "ai";
+import { z } from "zod";
 
 export const POST: APIRoute = async ({ request }) => {
   const openai = createOpenAI({
@@ -9,12 +10,32 @@ export const POST: APIRoute = async ({ request }) => {
   });
   const json = await request.json();
 
-  const { messages } = json as { messages: CoreMessage[] };
+  const { messages } = json as { messages: Message[] };
 
   if (messages.length) {
+    const coreMessages = convertToCoreMessages(messages);
+
     const response = streamText({
       model: openai("gpt-4o"),
-      messages,
+      messages: coreMessages,
+      maxSteps: 5,
+      tools: {
+        getWeather: {
+          description: "Get the current weather at a location",
+          parameters: z.object({
+            latitude: z.number(),
+            longitude: z.number(),
+          }),
+          execute: async ({ latitude, longitude }) => {
+            const response = await fetch(
+              `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m&hourly=temperature_2m&daily=sunrise,sunset&timezone=auto`,
+            );
+
+            const weatherData = await response.json();
+            return weatherData;
+          },
+        },
+      },
     });
 
     return response.toDataStreamResponse();
