b7ea9e7bba5e943b7cd1288bfd42e672bc6e8a92
DaniAkash <s.daniakash@gmail.com>
2024-11-26 20:04:49 +0530

    chore: added base layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/reveal.js",
+  ],
   theme: {
     extend: {
       borderRadius: {
