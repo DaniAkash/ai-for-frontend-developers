5f7f9926dad82fb88d78d79477230ba0a56c7a27
DaniAkash <s.daniakash@gmail.com>
2024-11-28 20:58:49 +0530

    feat: demo app complete

--- src/pages/api/demo.ts
+++ src/pages/api/demo.ts
@@ -1,6 +1,7 @@
 import type { APIRoute } from "astro";
 import { createOpenAI } from "@ai-sdk/openai";
 import { convertToCoreMessages, streamText } from "ai";
+import { z } from "zod";
 
 const openai = createOpenAI({
   compatibility: "strict",
@@ -18,6 +19,33 @@ export const POST: APIRoute = async ({ request }) => {
     const response = streamText({
       model: openai("gpt-4o"),
       messages: coreMessages,
+      tools: {
+        getWeather: {
+          description: "Get the current weather at a location",
+          parameters: z.object({
+            latitude: z.number(),
+            longitude: z.number(),
+          }),
+          execute: async ({ latitude, longitude }) => {
+            const response = await fetch(
+              `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m&hourly=temperature_2m&daily=sunrise,sunset&timezone=auto`,
+            );
+
+            const weatherData = await response.json();
+            return weatherData;
+          },
+        },
+        createCodeBlock: {
+          description: "Create a code block",
+          parameters: z.object({
+            code: z.string(),
+            language: z.string(),
+          }),
+          execute: async ({ code, language }) => {
+            return { code, language };
+          },
+        },
+      },
     });
     return response.toDataStreamResponse();
   }
